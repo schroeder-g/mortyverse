1d61f3f07124d610c10910de6c0de5730d2c77c0
Alex <schroedergoncalves@gmail.com>
2020-07-18 23:45:57 -0400

    Drafted Axios get

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'; //needs Rick n Morty graphics
+//also we should change the favicon
 import axios from 'axios'
 import Styled from 'styled-components'
+import {  BASE_URL,ARR_QRY_STRS , PAGIFICATION /*we should add our git & social info in there as well */} from './Constants'
 import './App.css';
 
+const StyledApp = Styled.section`
+
+`
+
 function App() {
+  // instantiate state: charDataReceived; locDataRetreived; epDdataReceived =({})...
+
+
+  //Grab all pages of Character dataset
+  //  useEffect (() => {
+  //   axios.get(`${BASE_URL} ${ARR_QRY_STRS[0]} ${PAGIFICATION}`)
+  //     .then ( res => {
+  //       setCharDataReceived
+  //     })
+  //     .catch ( err => {
+  //       console.log(err)
+  //     } )
+  // }, [])
   return (
-    <div className="App">
+    <StyledApp className="App">
      {/* Create Nav component */}
      {/* Create Marketing Page Component */}
      {/* Create  */}
-    </div>
+    </StyledApp>
   );
 }
 
---
+++ src/Constants/constants.js
@@ -0,0 +1,4 @@
+//HTMLpull queries for Rick n' Morty
+export const BASE_URL = "https://rickandmortyapi.com/api"
+export const ARR_QRY_STRS = ["character", "location", "episode"]
+export const PAGIFICATION = "?page="
