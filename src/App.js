4915ddc55bf80a99131a80e774bc8558f9846c98
Ashley <ashley.bergsma42@gmail.com>
2020-07-19 00:02:09 -0700

    began building character cards in separate component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import axios from 'axios'
-import Styled from 'styled-components'
+import styled from 'styled-components'
 import './App.css';
 
 function App() {
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,10 +2,23 @@ import React from 'react';
 import axios from 'axios'; 
 import styled from 'styled-components'
 
+// Establishing a character component to render a div container for each character in the Mortyverse
+
 function CharacterCard() {
 
     return (
-        <div></div>
+        <div>
+            <h3>Character Name</h3>
+            <img src='' alt='' />
+            <h4>Status: </h4>
+            <p>Species: </p>
+            <p>Origin: </p>
+            <p>Episodes: </p>
+            <button>Full Archive</button>
+            <ul>
+                <li>List of episodes character appears in</li>
+            </ul>
+        </div>
     )
 }
 
